106f2f4067611928939e4018733e7c831b3f3440
sarow <msarowar947@gmail.com>
2024-03-15 21:10:26 +0600

    changed

--- test.dart
+++ test.dart
@@ -1,8 +1,23 @@
-main(){
-
-  print('Sarowar');
-  print('Sadhen');
-  // skdskl ;
-  print('hi dart i used you ');
-  print(" hi woerld i am md sarowar " );
+class Media{
+   void play(){
+    print('Playing media...');
+  }
 }
+class Song extends Media{
+  String artist ='(james guru)';
+  @override
+  play(){
+  print('Playing song by $artist');
+  }
+}
+
+void main() {
+  Media media = Media();
+  Song song = Song();
+  media.play();
+  song.play();
+}
+
+
+
+
